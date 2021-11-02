b60c0f0d6a89f41d0ad9123ba935477ede6023a2
harshrawat20 <77675473+harshrawat20@users.noreply.github.com>
2021-11-02 20:28:55 +0530

    Practice day 1

---
+++ array/Longest Span Sum in two arrays.cpp
@@ -0,0 +1,32 @@
+// A simple C++ program to find longest common subarray of two binary arrays with same sum
+#include<bits/stdc++.h>
+using namespace std;
+int longestCommonSum(bool arr1[],bool arr2[],int n)
+{
+    int maxLen=0;
+    for(int i=0;i<n;i++)
+    {
+        int sum1=0,sum2=0;
+        for(int j=i;j<n;j++)
+        {
+            sum1+=arr1[j];
+            sum2+=arr2[j];
+            if(sum1==sum2)
+            {
+                int len=j-i+1;
+                if(len>maxLen)
+                maxLen=len;
+            }
+        }
+        
+    }
+    return maxLen;
+}
+int main()
+{
+bool arr1[]={0,1,0,1,1,1,1};
+bool arr2[]={1,1,1,1,1,0,1};
+int n=sizeof(arr1)/sizeof(arr1[0]);
+cout<< "Length of the longest common span with same sum is "<<longestCommonSum(arr1, arr2, n)<<endl;
+return 0;
+}
